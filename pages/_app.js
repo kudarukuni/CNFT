9c1b672fbc8d138ef7e0982855b05b259fac6477
Kudakwashe Rukuni <krukuni@gmail.com>
2023-01-24 16:33:39 +0200

    booo

---
+++ pages/_app.js
@@ -0,0 +1,30 @@
+import { useMemo } from "react";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { ConnectionProvider, WalletProvider, useWallet } from "@solana/wallet-adapter-react";
+import "../styles/App.css";
+//import "../styles/index.css";
+import "../styles/globals.css";
+import "../styles/CandyMachine.css";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+const App = ({ Component, pageProps }) => {
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(() => [new PhantomWalletAdapter(), new SlopeWalletAdapter(), new SolflareWalletAdapter(), new TorusWalletAdapter()], [network]);
+  const wallet = useWallet();
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import CandyMachine from "../components/CandyMachine";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
+const TWITTER_HANDLE = "Naswillow";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const Home = () => {
+  const wallet = useWallet();
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/h5NLPVn3rg0Rq/giphy.gif" alt="emoji" />
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🍭 Goritoto Drop</p>
+          <p className="sub-text">The official Goritoto NFT Metaplex Candy Machine with fair mint</p>
+          {wallet.publicKey ? <CandyMachine walletAddress={wallet} /> : renderNotConnectedContainer()}
+        </div>
+
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+          <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`Designed and Developed By ${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
