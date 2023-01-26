e13eeb75c70b2fa06a313ac1c901cc11d42ecc95
Kudakwashe Rukuni <krukuni@gmail.com>
2023-01-26 13:26:08 +0200

    new things

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,19 @@ import { useMemo } from "react";
 import { clusterApiUrl } from "@solana/web3.js";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from "@solana/wallet-adapter-wallets";
-import { ConnectionProvider, WalletProvider, useWallet } from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+
 import "../styles/App.css";
 import "../styles/globals.css";
 import "../styles/CandyMachine.css";
+import "../styles/CountdownTimer.css";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
 const App = ({ Component, pageProps }) => {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  const wallets = useMemo(() => [new PhantomWalletAdapter(), new SlopeWalletAdapter(), new SolflareWalletAdapter(), new TorusWalletAdapter()], [network]);
-  const wallet = useWallet();
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
