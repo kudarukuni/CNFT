7206ae0109802beca5315bff3e9c047fdd7bed6d
kudarukuni <krukuni@gmail.com>
2023-02-19 13:44:28 +0200

    new candymachine v2 code

--- pages/index.js
+++ pages/index.js
@@ -3,41 +3,45 @@ import dynamic from "next/dynamic";
 import CandyMachine from "../components/CandyMachine";
 import { useWallet } from "@solana/wallet-adapter-react";
 
+// This is to disable SSR when using WalletMultiButton
 const WalletMultiButtonDynamic = dynamic(
   async () => (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
   { ssr: false }
 );
 
-const TWITTER_HANDLE = "Naswillow";
+// Constants
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
-const Home = () => {
-  const wallet = useWallet();
-  const renderNotConnectedContainer = () => (
-    <div>
-      <img src="https://media.giphy.com/media/h5NLPVn3rg0Rq/giphy.gif" alt="emoji" />
-      <div className="button-container">
-        <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
-      </div>
-    </div>
-  );
+cconst Home = () => {
+    const wallet = useWallet();
+    // Actions
+    const renderNotConnectedContainer = () => (
+        <div>
+            <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
 
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">Goritoto Drop</p>
-          <p className="sub-text">The official Goritoto NFT Metaplex Candy Machine with fair mint</p>
-          {wallet.publicKey ? <CandyMachine walletAddress={wallet} /> : renderNotConnectedContainer()}
+            <div className="button-container">
+                <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
+            </div>
         </div>
+    );
 
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
-          <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`Designed and Developed By ${TWITTER_HANDLE}`}</a>
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="header-container">
+                    <p className="header">🍭 Candy Drop</p>
+                    <p className="sub-text">NFT drop machine with fair mint</p>
+                    {/* Render your connect to wallet button right here */}
+                    {wallet.publicKey ? <CandyMachine walletAddress={wallet} /> : renderNotConnectedContainer()}
+                </div>
+                <div className="footer-container">
+                    <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+                    <a className="footer-text" href={TWITTER_LINK} target="_blank" rel="noreferrer">{`built on @${TWITTER_HANDLE}`}</a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
